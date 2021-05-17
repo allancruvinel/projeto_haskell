8aabfe62d8f0846baafbf6907c19b019bc78ae73
Allan Cruvinel <allan.cruvinel@gmail.com>
2021-05-17 18:36:41 -0300

    primeiro commit

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -1,14 +1,94 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Handler.Home where
 
 import Import
-import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
+
+--import Network.HTTP.Types.Status
+--import Database.Persist.Postgresql
+
+getPage2R :: Handler Html
+getPage2R = do
+  defaultLayout $ do
+    toWidgetHead
+      [julius|
+
+       function ola(){
+         alert("oi");
+       }
+    |]
+    toWidgetHead
+      [cassius|
+
+         h1 
+            color : red;
+    |]
+    [whamlet|
+        <h1>
+            Page2R
+        <button onclick="ola()">
+            OK
+    |]
+
+getPage1R :: Handler Html
+getPage1R = do
+  defaultLayout $ do
+    toWidgetHead
+      [julius|
+
+       function ola(){
+         alert("oi");
+       }
+    |]
+    toWidgetHead
+      [cassius|
+
+         h1 
+            color : red;
+    |]
+    [whamlet|
+        <h1>
+            Page1R
+
+        <img src=@{StaticR img_image_jpg}>
+            
+        <button onclick="ola()">
+            OK
+    |]
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+  defaultLayout $ do
+    toWidgetHead
+      [julius|
+
+       function ola(){
+         alert("oi");
+       }
+    |]
+    toWidgetHead
+      [cassius|
+
+         h1 
+            color : red;
+    |]
+    [whamlet|
+        <h1>
+            Home
+
+        <ul>
+            <li>
+                <a href=@{Page1R}>
+                    Pagina 1
+            <li>
+                <a href=@{Page2R}>
+                    Pagina 2    
+
+        <button onclick="ola()">
+            OK
+    |]
